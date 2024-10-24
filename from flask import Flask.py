1f74e199bc9577dd5ce1805dda622f557a9e2bb4
Dman0808 <168228320+Dman0808@users.noreply.github.com>
2024-10-24 11:22:10 -0500

    Fix route decorator and update Flask version to 1.1.2

--- from flask import Flask.py
+++ from flask import Flask.py
@@ -1,6 +1,6 @@
 from flask import Flask
 app = Flask(1.1.2)
 
-@app.route(https://thepathwaypioneer.com/)
+@app.route(https://thepathwaypioneer.com/C:User/dus08/OneDrive/Documents/Github/AutoGPT/autogpt_platform/autogpt_libs/pyproject.toml)
 def hello():
     return "Hello World!"
