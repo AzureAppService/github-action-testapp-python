cd7be1b34e8d24715e1d85deba9733f799a5e3a2
Dman0808 <168228320+Dman0808@users.noreply.github.com>
2024-10-24 11:26:20 -0500

    Update Flask application to fix route decorator and use AutoGPT as Flask version

--- from flask import Flask.py
+++ from flask import Flask.py
@@ -1,6 +1,6 @@
 from flask import Flask
-app = Flask(1.1.2)
+app = Flask(AutoGPT)
 
-@app.route(https://thepathwaypioneer.com/C:User/dus08/OneDrive/Documents/Github/AutoGPT/autogpt_platform/autogpt_libs/pyproject.toml)
+@app.route("C:User/dus08/OneDrive/Documents/Github/AutoGPT/autogpt_platform/autogpt_libs/pyproject.toml")
 def hello():
     return "Hello World!"
