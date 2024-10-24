c5569acfa715b1524ea1a5c6e5497616385aa686
Dman0808 <168228320+Dman0808@users.noreply.github.com>
2024-10-24 11:45:27 -0500

    Fix import errors and update Flask version

--- from flask import Flask.py
+++ from flask import Flask.py
@@ -1,6 +1,6 @@
-from wsgiref.util import application_uri
-from flask import Flask
-app = Flask(AutoGPT)
+from wsgiref.util import application_uri  # noqa: F401
+from flask import Flask # type: ignore
+app = Flask(AutoGPT)  # type: ignore # noqa: F821
 
 @app.route("C:User/dus08/OneDrive/Documents/Github/AutoGPT/autogpt_platform/autogpt_libs/pyproject.toml")
 def hello():
